689a1970a3e55c149aa6b7b204953d05d104b0be
Tristan Labelle <3llianx@gmail.com>
2014-01-11 06:17:46 -0800

    Added members to the IDocument interface.

--- thrift_service/BlitzIndex/BlitzIndex/IDocument.cs
+++ thrift_service/BlitzIndex/BlitzIndex/IDocument.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using com.coveo.blitz.thrift;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Thrift.Collections;
 
 namespace BlitzIndex
 {
 	public interface IDocument
 	{
+		string Id { get; }
+		DocumentType Type { get; }
 		IEnumerable<string> Keywords { get; }
+
+		THashSet<string> GetFacet(string name);
 	}
 }
