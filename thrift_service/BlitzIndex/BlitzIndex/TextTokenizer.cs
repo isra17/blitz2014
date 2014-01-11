121f997eeae5dc43846f0963c1037f3c8801743e
Tristan Labelle <3llianx@gmail.com>
2014-01-11 05:55:42 -0800

    Implemented text tokenization.

---
+++ thrift_service/BlitzIndex/BlitzIndex/TextTokenizer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BlitzIndex
+{
+	public static class TextTokenizer
+	{
+		private static readonly Regex tokenizeRegex = new Regex(
+			@"[\p{L}-_\d]+", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+		public static IEnumerable<string> Tokenize(string text)
+		{
+			Contract.Requires(text != null);
+
+			Match match = tokenizeRegex.Match(text);
+			while (match.Success)
+			{
+				bool valid = false;
+				foreach (var c in match.Value)
+				{
+					if (c != '-' && c != '_')
+					{
+						valid = true;
+						break;
+					}
+				}
+
+				if (valid) yield return match.Value;
+
+				match = match.NextMatch();
+			}
+		}
+	}
+}
