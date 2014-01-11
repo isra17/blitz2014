b6db9d6a6b52d8b7485af97aea10700da279c856
Tristan Labelle <3llianx@gmail.com>
2014-01-11 10:50:53 -0800

    Implemented full-text search.

--- thrift_service/BlitzIndex/BlitzIndex/QueryEvaluator.cs
+++ thrift_service/BlitzIndex/BlitzIndex/QueryEvaluator.cs
@@ -53,11 +53,40 @@ namespace com.coveo.blitz.thrift
                 .Select(pair => pair.Key);
         }
 
+		private bool MatchesFullText(SearchResult result, string[] words, int occurenceIndex)
+		{
+			int nextTokenStartIndex = occurenceIndex + words[0].Length;
+			for (int wordIndex = 1; wordIndex < words.Length; ++wordIndex)
+			{
+				var token = TextTokenizer.GetNextToken(result.Document.Text, nextTokenStartIndex);
+				if (!token.HasValue || token.Value.Value != words[wordIndex])
+					return false;
+			}
+
+			return true;
+		}
+
+		private bool MatchesFullText(SearchResult result, string[] words)
+		{
+			// At least one of the occurence should also match the other words
+			foreach (var occurenceIndex in result.Occurrences)
+				if (MatchesFullText(result, words, occurenceIndex))
+					return true;
+			return false;
+		}
+
         private HashSet<SearchResult> FindDocuments(Database db, QueryTreeNode specificNode)
         {
             if (specificNode.Type == NodeType.LITERAL)
             {
-                return db.Query(specificNode.Value);
+				var words = TextTokenizer.Tokenize(specificNode.Value).Select(t => t.Value).ToArray();
+				if (words.Length == 0) return new HashSet<SearchResult>();
+
+				var results = db.Query(words[0]);
+				if (words.Length > 1)
+					results.RemoveWhere(result => !MatchesFullText(result, words));
+
+				return results;
             }
 
             var operatorName = specificNode.Value.ToUpperInvariant().Trim();
