f8008f2b791df9f68d43136e37dac26d2751c347
Israel Hallé <isra017@gmail.com>
2014-01-11 09:23:46 -0500

    Added database class

---
+++ thrift_service/BlitzIndex/BlitzIndex/indexer/Database.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace BlitzIndex
+{
+	public class Database
+	{
+		Dictionary<int, int> m_entries;
+		
+		public Database ()
+		{
+			
+		}
+		
+		public void insert()
+		{
+			
+		}
+		
+		public void query()
+		{
+			
+		}			
+	}
+}
+
